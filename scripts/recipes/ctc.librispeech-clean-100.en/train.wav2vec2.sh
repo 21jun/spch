39ea5171feb02ecea867db0f4029939f8a130f2f
21jun <21jun7654@gmail.com>
2025-01-09 17:49:17 +0900

    uv run

--- scripts/recipes/ctc.librispeech-clean-100.en/train.wav2vec2.sh
+++ scripts/recipes/ctc.librispeech-clean-100.en/train.wav2vec2.sh
@@ -1,4 +1,4 @@
-accelerate launch --num_processes $1 src/train/train_ctc.py --config-path ../../scripts/recipes/ctc.librispeech-clean-100.en/conf --config-name librispeech-ctc \
+uv run accelerate launch --num_processes $1 src/train/train_ctc.py --config-path ../../scripts/recipes/ctc.librispeech-clean-100.en/conf --config-name librispeech-ctc \
 ++train.per_device_train_batch_size=16 \
 ++train.gradient_accumulation_steps=2 \
 ++train.learning_rate=1e-6 \
