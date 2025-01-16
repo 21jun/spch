51c9e499d193bce933520d487ebe19473e56b995
21jun <21jun7654@gmail.com>
2025-01-16 17:05:28 +0900

    config for ksponspeech

--- scripts/recipes/seq.whisper.librispeech-clean-100.en/train.whisper-large-v3-turbo.sh
+++ scripts/recipes/seq.whisper.librispeech-clean-100.en/train.whisper-large-v3-turbo.sh
@@ -1,1 +1,8 @@
-accelerate launch --num_processes $1 src/train/train_seq2seq.py --config-path ../../scripts/recipes/seq.whisper.librispeech-clean-100.en/conf --config-name librispeech-seq-whisper
+PYTHONPATH=. uv run accelerate launch --num_processes $1 src/train/train_seq2seq.py --config-path ../../scripts/recipes/seq.whisper.librispeech-clean-100.en/conf --config-name librispeech-seq-whisper \
+model=whisper-large-v3-turbo \
+++train.per_device_train_batch_size=4 \
+++train.per_device_eval_batch_size=4 \
+++train.gradient_accumulation_steps=2 \
+++train.learning_rate=5e-6 \
+++train.num_epochs=100 \
+++train.eval_steps=500
--- scripts/recipes/seq.whisper.librispeech-clean-100.en/train.whisper-tiny.sh
+++ scripts/recipes/seq.whisper.librispeech-clean-100.en/train.whisper-tiny.sh
@@ -1,7 +1,8 @@
-PYTHONPATH=. uv run accelerate launch --num_processes $1 src/train/train_seq2seq.py --config-path ../../scripts/recipes/seq.whisper.librispeech-clean-100.en/conf --config-name librispeech-seq-whisper \
-++train.per_device_train_batch_size=32 \
-++train.per_device_eval_batch_size=32 \
-++train.gradient_accumulation_steps=1 \
-++train.learning_rate=1e-4 \
+PYTHONPATH=. uv run accelerate launch --main_process_port $((30000 + RANDOM % 20001)) --num_processes $1 src/train/train_seq2seq.py --config-path ../../scripts/recipes/seq.whisper.librispeech-clean-100.en/conf --config-name librispeech-seq-whisper \
+model=whisper-tiny \
+++train.per_device_train_batch_size=16 \
+++train.per_device_eval_batch_size=16 \
+++train.gradient_accumulation_steps=2 \
+++train.learning_rate=5e-6 \
 ++train.num_epochs=100 \
-++train.eval_steps=10
+++train.eval_steps=100
